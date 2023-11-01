8836a5023e75c51c8579696ad4b5950d8ee884d2
Sisafokker <28725019+Sisafokker@users.noreply.github.com>
2023-11-01 20:52:00 +0100

    new home screen. Added scss. New pages

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,17 @@
+import React from "react";
+import "../styles/home.scss";
+
 function Home() {
-    return <div>
-       <h1>Welcome to React_Clasp</h1>
+  return (
+    <div className="home-wrapper"> {/* Add className for styling */}
+      <h1>Welcome to React_Clasp</h1>
+      <h3>Please Sign In to continue</h3>
+      <img className="custom-image"
+        src="https://lh3.googleusercontent.com/17JVDv4jno5isn9OoNozJ-ghj_46GKB3mrcu7cHY9EVIG-gI3EYwUyL0BiFOv1cbEzNWTbmr2GfEcgT14Vk3EXhaKmp9rk1l2AGeaOgSLBoQy5NiUHj_xs3BKmpMLOkpD7lK6UoExw=w2400"
+        alt="Hoakeen"
+      />
     </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/components/inventory.js
@@ -0,0 +1,11 @@
+function Inventory() {
+    return <div>
+       <h1>Inventory...</h1>
+       <h3>Ideas:</h3>
+       <ul>
+        <li></li>
+       </ul>
+    </div>
+}
+
+export default Inventory
