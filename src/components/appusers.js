f279a5fb46ccd92ce9235ccae879e878fd75ebe5
Sisafokker <28725019+Sisafokker@users.noreply.github.com>
2023-11-05 10:56:54 +0100

    Props and Handleing Form Success and Unsuccess

--- src/components/appusers.js
+++ src/components/appusers.js
@@ -1,40 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import AppUsersForm from "./appusersform";
+import AppusersForm from "./appusersform";
+
 function Appusers() {
     const [app_users, setApp_users] = useState([]);
 
     useEffect(() => {
-        // Make API Request to Fetch Student Data
+        fetchAppUsers();
+    }, []); // Fetch data on initial component mount
+
+    // Get Student Data
+    const fetchAppUsers = () => {
         axios.get(`http://localhost:${process.env.REACT_APP_PORT}/api/students`)
-          .then(response => {
-            setApp_users(response.data);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }, []);
+            .then(response => {
+                setApp_users(response.data);
+                console.log("👍students")
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    // After a new user is added
+    const handleUserAdded = () => {
+        fetchAppUsers();
+    };
 
     return <div>
-    <div>
-       <h1>AppUsers Page...</h1>
-       <h3>Ideas:</h3>
-       <ul>
-        <li>Google API admin console</li>
-        <li>CRUD Google Workspace Users</li>
-        <li>CRUD Google Cloud SQL Database</li>
-       </ul>
-    </div>
-    <div>
-      <h1>SQL AppUsers List</h1>
-      <AppUsersForm />
-      <ul>
-        {app_users.map(u => (
-          <li key={u.students_id}>{u.students_lastName}, {u.students_firstName} 📧 {u.students_email} </li>
-        ))}
-      </ul>
-    </div>
+        <div>
+            <h1>AppUsers Page...</h1>
+            <h3>Ideas:</h3>
+            <ul>
+                <li>CRUD Google Cloud SQL Database</li>
+                <li>Google API admin console</li>
+                <li>CRUD Google Workspace Users</li>
+            </ul>
+        </div>
+        <div>
+            <AppusersForm prop_handleUserAdded={handleUserAdded} /> {/* 'prop_handleUserAdded' prop passed to 'AppUsersForm' */}
+            <h1>[SQL] Current AppUsers:</h1>
+            <ul>
+                {app_users.map(u => (
+                    <li key={u.students_id}>{u.students_lastName}, {u.students_firstName} 📧 {u.students_email} </li>
+                ))}
+            </ul>
+        </div>
     </div>
 }
 
--- src/components/appusersform.js
+++ src/components/appusersform.js
@@ -1,49 +1,68 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const AppUsersForm = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+const AppusersForm = ({ prop_handleUserAdded }) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [formError, setFormError] = useState(null);
+    const [formSuccess, setFormSuccess] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Make API Request to Add AppUser
-    axios.post(`http://localhost:${process.env.REACT_APP_PORT}/api/students`, {
-        students_firstName: firstName,
-        students_lastName: lastName,
-        students_email: email
-    })
-      .then(response => {
-        console.log('AppUser added successfully:', response.data);
-        // Handle success, e.g., redirect to the appusers list page
-      })
-      .catch(error => {
-        console.error(error);
-        // Handle error, show error message to the user
-      });
-  };
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
 
-  return (
-    <div>
-      <h2>Add AppUser</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        </label>
-        <label>
-          Email:
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <button type="submit">Add AppUser</button>
-      </form>
-    </div>
-  );
+        axios.post(`http://localhost:${process.env.REACT_APP_PORT}/api/students`, {
+            students_firstName: firstName,
+            students_lastName: lastName,
+            students_email: email
+        })
+            .then(response => {
+                console.log('AppUser added successfully:', response.data);
+                prop_handleUserAdded();// Call the prop_handleUserAdded prop to trigger a refresh of the App Users list
+                clearForm(); // Clear form
+            })
+            .catch(error => {
+                console.error(error);
+                setFormError('Form Failed. Please try again.'); 
+            });
+    };
+
+    const clearForm = () => {
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setFormError(null);
+        setFormSuccess("👍 New Users was added");
+    }
+
+    return (
+        <div>
+            <h2>[SQL] Add AppUser</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    <label>
+                        First Name:
+                        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    </label>
+                    <label>
+                        Last Name:
+                        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    </label>
+                </div>
+                <div>
+                <label>
+                    Email:
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                </div>
+                <div>
+                {formError && <div className='errorWarning'>{formError}</div>}
+                {formSuccess && <div className='successWarning'>{formSuccess}</div>}
+                <button type="submit">Add AppUser</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
-export default AppUsersForm;
+export default AppusersForm;
