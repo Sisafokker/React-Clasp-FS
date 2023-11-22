84d4eb8109b1b58ab18d03a0ce41a77cba6272e8
Sisafokker <28725019+Sisafokker@users.noreply.github.com>
2023-11-22 17:50:23 +0100

    v0.4.1

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,7 +16,14 @@ const Footer = () => {
         </div>
         <div className="footer-Col">
           {/* <h4>Second Column</h4> */}
-          © { new Date().getFullYear() } - Hoakeen - Full Stack Project
+          <img className="custom-image"
+            src="https://lh3.googleusercontent.com/17JVDv4jno5isn9OoNozJ-ghj_46GKB3mrcu7cHY9EVIG-gI3EYwUyL0BiFOv1cbEzNWTbmr2GfEcgT14Vk3EXhaKmp9rk1l2AGeaOgSLBoQy5NiUHj_xs3BKmpMLOkpD7lK6UoExw=w70"
+            alt="Hoakeen"
+          />
+        </div>
+        <div className="footer-Col">
+          {/* <h4>Third Column</h4> */}
+          © { new Date().getFullYear() } - <a href="http://www.hoakeen.com" target="_blank">Hoakeen</a> - FS Project
         </div>
       </div>
     </div>
--- src/components/footer_info.js
+++ src/components/footer_info.js
@@ -1,8 +1,9 @@
 // footer_info.js
 const Info = Object.freeze({
   notes: [
-    {
-      version: "v0.4.0", description: "SQL Updates",  details: [
+    { version: "v0.4.2", description: "TBD", details: ["1", "2", "3", "4", "5",],  },
+    { version: "v0.4.1", description: "Footer Visual Changes", details: ["Added Logo to Footer", "Added 3rd column to footer",],  },
+    { version: "v0.4.0", description: "SQL Updates", details: [
         "Added SQL Intermediary Table intCompanyUsers",
         "Added IntCompanyUsers to Server.js",
         "You can create Appusers and assign them to a Company",
@@ -10,25 +11,24 @@ const Info = Object.freeze({
         "Cleaned Scss a bit more",
       ],
     },
-    { version: "v0.3.0", description: "UI/UX Improvements", details: [ "Nightmare SCSS with Login Section", ],  },
-    { version: "v0.2.0", description: "Styling Updates", details: [ "Nightmare with SCSS", ],  },
-    { version: "v0.1.0", description: "Feature Introduction", details: [ "Displaying Version Data",],  },
-    { version: "v0.0.0", description: "Initial Release", details: [ "Initial release", ], }
+    { version: "v0.3.0", description: "UI/UX Improvements", details: ["Nightmare SCSS with Login Section",], },
+    { version: "v0.2.0", description: "Styling Updates", details: ["Nightmare with SCSS",], },
+    { version: "v0.1.0", description: "Feature Introduction", details: ["Displaying Version Data",] },
+    { version: "v0.0.0", description: "Initial Release", details: ["Initial release (if this info section)",], }
   ],
-  
-  
-    project: {
-      name: 'Bottega Final Project: Hoakeen',
-      version: {
-        major: 0,
-        minor: 4,
-        patch: 0,
-        toString() {
-          return `${this.major}.${this.minor}.${this.patch}`;
-        }
+
+  project: {
+    name: 'Bottega Final Project: Hoakeen',
+    version: {
+      major: 0,
+      minor: 4,
+      patch: 0,
+      toString() {
+        return `${this.major}.${this.minor}.${this.patch}`;
       }
-    },
-  
+    }
+  },
+
   getLatestNote() {
     const latestNote = this.notes[0];
     return latestNote;
@@ -39,6 +39,5 @@ const Info = Object.freeze({
     return this.project.version.toString();
   },
 });
-  
-  export default Info;
-  
+
+export default Info;
