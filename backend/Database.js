9af23e0fe17796ed2d698bfdc4f369010bbf3c80
Sisafokker <28725019+Sisafokker@users.noreply.github.com>
2023-11-25 17:13:11 +0100

    v0.6.0 Refactored Entire Backend- Server.js, xxxManager.js

--- backend/Database.js
+++ backend/Database.js
@@ -3,12 +3,12 @@ const mysql = require('mysql');
 class Database {
   constructor() {
     /* LOCAL EVERYTHING ⌛⌛DEVELOPMENT ENVIRONMENT⌛⌛*/ 
-    // this.pool = mysql.createPool({
-    //   host: process.env.SQL_HOST,
-    //   user: process.env.SQL_USER,
-    //   password: process.env.SQL_PASSWORD,
-    //   database: process.env.SQL_DATABASE
-    // });
+    this.pool = mysql.createPool({
+      host: process.env.SQL_HOST,
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      database: process.env.SQL_DATABASE
+    });
 
     /* LOCAL BACKEND - CLOUD SQL*/ 
     // this.pool = mysql.createPool({
@@ -20,12 +20,12 @@ class Database {
 
 
     /* CLOUD BACKEND && CLOUD SQL 🚩🚩USE THESE ONLY WHEN UPDATING CLOUD ENGINE🚩🚩 */ 
-    this.pool = mysql.createPool({
-    user: process.env.GOOGLE_SQL_USER,
-    password: process.env.GOOGLE_SQL_PASSWORD,
-    database: process.env.GOOGLE_SQL_DATABASE,
-    socketPath: process.env.GOOGLE_SQL_DB_HOST
-    });
+    // this.pool = mysql.createPool({
+    // user: process.env.GOOGLE_SQL_USER,
+    // password: process.env.GOOGLE_SQL_PASSWORD,
+    // database: process.env.GOOGLE_SQL_DATABASE,
+    // socketPath: process.env.GOOGLE_SQL_DB_HOST
+    // });
 }
 
   query(sql, params, tableName) {
